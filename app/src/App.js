c33ea0a77f3bd8b8354af9948edd3820b0ebcb7d
Gary Rios <78942879+babypwant@users.noreply.github.com>
2021-09-02 13:45:35 -0700

    working build

--- app/src/App.js
+++ app/src/App.js
@@ -2,15 +2,39 @@ import { useState } from "react";
 const txml = require('txml');
 
 const App = () => {
-  const [name, setName] = useState(0);
-  const [selectedFile, setSelectedFile] = useState("");
-  let data;
+  const [name, setName] = useState("");
+  let data = [['property_id', 'account_number', 'name', 'address1', 'city', 'state_prov', 'postal_code', 'primary_contact_id ']];
+  let xmlFile;
   let fileReader;
 
+  //I want to make data an array with all info inside of the array
   const handleFileRead = () => {
     const content = fileReader.result;
-    data = txml.parse(content)
-    console.log(data);
+    xmlFile = txml.simplify(txml.parse(content))
+    xmlFile.Table_Facility.T_Facility.forEach((facility) => {
+      let metadata = [];
+      facility.Facility_ID ? metadata.push(facility.Facility_ID) : metadata.push("none");
+      facility.Facility_Account_Number ? metadata.push(facility.Facility_Account_Number) : metadata.push("none");
+      facility.Facility_Name ? metadata.push(facility.Facility_Name) : metadata.push("none");
+      facility.Service_Address_Full ? metadata.push(facility.Service_Address_Full) : metadata.push("none");
+      facility.Service_Address_City ? metadata.push(facility.Service_Address_City) : metadata.push("none");
+      facility.Service_Address_State ? metadata.push(facility.Service_Address_State) : metadata.push("none");
+      facility.Service_Address_Zip_Code ? metadata.push(facility.Service_Address_Zip_Code) : metadata.push("none");
+      facility.Facility_Contact_Mgr_ID ? metadata.push(facility.Facility_Contact_Mgr_ID) : metadata.push("none");
+      data.push(metadata);
+    });
+    let csvContent = ""
+      + data.map(e => e.join(",")).join("\n");
+    console.log(csvContent)
+
+    let csvData = new Blob([csvContent], { type: 'text/csv' });
+    let csvUrl = URL.createObjectURL(csvData);
+    let link = document.createElement('a');
+    link.href = csvUrl;
+    link.target = '_blank';
+    link.download = "converted" + '.csv';
+    link.click();
+
   };
 
   const handleUpload = (file) => {
@@ -30,7 +54,6 @@ const App = () => {
 
         <input
           type="file"
-          value={selectedFile}
           onChange={(e) => handleUpload(e.target.files[0])}
         />
       </form>
