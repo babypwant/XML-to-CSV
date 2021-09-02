57efec4b183277252bf19edb4574a6bbe7be0887
Gary Rios <78942879+babypwant@users.noreply.github.com>
2021-09-02 15:35:54 -0700

    updates

--- app/src/App.js
+++ app/src/App.js
@@ -3,30 +3,34 @@ const txml = require('txml');
 
 const App = () => {
   const [name, setName] = useState("");
-  let data = [['property_id', 'account_number', 'name', 'address1', 'city', 'state_prov', 'postal_code', 'primary_contact_id ']];
+  let data = [['property_id', 'account_number', 'name', 'address1', 'address1', 'city', 'state_prov', 'postal_code', 'primary_contact_id ']];
   let xmlFile;
   let fileReader;
 
-  //I want to make data an array with all info inside of the array
   const handleFileRead = () => {
     const content = fileReader.result;
     xmlFile = txml.simplify(txml.parse(content))
     xmlFile.Table_Facility.T_Facility.forEach((facility) => {
       let metadata = [];
-      facility.Facility_ID ? metadata.push(facility.Facility_ID) : metadata.push("none");
-      facility.Facility_Account_Number ? metadata.push(facility.Facility_Account_Number) : metadata.push("none");
-      facility.Facility_Name ? metadata.push(facility.Facility_Name) : metadata.push("none");
-      facility.Service_Address_Full ? metadata.push(facility.Service_Address_Full) : metadata.push("none");
-      facility.Service_Address_City ? metadata.push(facility.Service_Address_City) : metadata.push("none");
-      facility.Service_Address_State ? metadata.push(facility.Service_Address_State) : metadata.push("none");
-      facility.Service_Address_Zip_Code ? metadata.push(facility.Service_Address_Zip_Code) : metadata.push("none");
-      facility.Facility_Contact_Mgr_ID ? metadata.push(facility.Facility_Contact_Mgr_ID) : metadata.push("none");
+      console.log(facility)
+      facility.Facility_ID ? metadata.push(facility.Facility_ID) : metadata.push("none"); //property_id
+      facility.Facility_Account_Number ? metadata.push(facility.Facility_Account_Number) : metadata.push("none"); //account_number
+      facility.Facility_Name ? metadata.push(facility.Facility_Name) : metadata.push("none"); //name
+      facility.Service_Address_Full ? metadata.push(facility.Service_Address_Full) : metadata.push("none"); //address1
+      metadata.push(""); //add address2 ? cannot be found, testing empty string in its place
+      facility.Service_Address_City ? metadata.push(facility.Service_Address_City) : metadata.push("none"); //city
+      facility.Service_Address_State ? metadata.push(facility.Service_Address_State) : metadata.push("none"); //state
+      metadata.push("US") //country code
+      facility.Service_Address_Zip_Code ? metadata.push(facility.Service_Address_Zip_Code) : metadata.push("none"); //postal_code
+      facility.Facility_Contact_Mgr_ID ? metadata.push(facility.Facility_Contact_Mgr_ID) : metadata.push("none"); //primary contact Id
+      facility.Facility_Comments_01 ? metadata.push(facility.Facility_Comments_01) : metadata.push(""); //notes
+       //survery_complicance
+      //last_survey
+      //next_survey
       data.push(metadata);
     });
     let csvContent = ""
       + data.map(e => e.join(",")).join("\n");
-    console.log(csvContent)
-
     let csvData = new Blob([csvContent], { type: 'text/csv' });
     let csvUrl = URL.createObjectURL(csvData);
     let link = document.createElement('a');
