664fcb385720e0b28a0ba7bb7254d119152787b1
Gary Rios <78942879+babypwant@users.noreply.github.com>
2021-09-01 22:31:34 -0700

    progress for tonight

--- app/src/App.js
+++ app/src/App.js
@@ -1,19 +1,44 @@
 import { useState } from "react";
-function App() {
-  const [file, setFile] = ({ });
-  const getData = () => {
-    console.log("here")
+const txml = require('txml');
+
+const App = () => {
+  const [name, setName] = useState(0);
+  const [selectedFile, setSelectedFile] = useState("");
+  let data;
+  let fileReader;
+
+  const handleFileRead = () => {
+    const content = fileReader.result;
+    data = txml.parse(content)
+    console.log(data);
+  };
+
+  const handleUpload = (file) => {
+    fileReader = new FileReader();
+    fileReader.onloadend = handleFileRead;
+    fileReader.readAsText(file);
   };
 
   return (
-    <div>
-      <h1>Hello from App</h1>
-      <input
-        onChange={getData}
-        type="file">
-      </input>
+    <div className="App">
+      <form>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+
+        <input
+          type="file"
+          value={selectedFile}
+          onChange={(e) => handleUpload(e.target.files[0])}
+        />
+      </form>
+      <div>
+
+      </div>
     </div>
   );
-}
+};
 
 export default App;
